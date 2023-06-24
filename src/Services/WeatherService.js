89fef3edb4e332ad0a86f655982630401911a26d
sagorpaul142 <sagorpaulcse@gmail.com>
2023-06-24 14:00:10 +0600

    api call and data show in ui

---
+++ src/Services/WeatherService.js
@@ -0,0 +1,71 @@
+import {DateTime} from "luxon";
+
+export const API_KEY = '55a39bc0eea8442c77458cb6e4dcf5dd';
+export const BASE_URL = "https://api.openweathermap.org/data/2.5/";
+
+export const getWeatherData = (infoType) => {
+  return fetch(BASE_URL + infoType + `&appid=${API_KEY}`).then(response => response.json())
+}
+const formatCurrentWeather = (data) => {
+  const {
+    coord: {lat, lon},
+    main: {temp, feels_like, temp_min, temp_max, humidity},
+    name,
+    dt,
+    sys: {country, sunrise, sunset},
+    weather,
+    wind: {speed}
+  } = data
+  const {main: details, icon} = weather[0]
+
+  return {
+    lat,
+    lon,
+    temp,
+    temp_min,
+    temp_max,
+    feels_like,
+    humidity,
+    name,
+    dt,
+    country,
+    sunrise,
+    sunset,
+    details,
+    icon,
+    speed
+  }
+}
+
+const formateForcastWeather = (data) => {
+  let {timezone, daily, hourly} = data
+  daily = daily.slice(1, 6).map(d => {
+    return {
+      title: formatToLocalTime(d.dt, timezone, 'ccc'),
+      temp: d.temp.day,
+      icon: d.weather[0].icon,
+    }
+  })
+  hourly = hourly.slice(1, 6).map(d => {
+    return {
+      title: formatToLocalTime(d.dt, timezone, 'hh:mm a'),
+      temp: d.temp,
+      icon: d.weather[0].icon
+    }
+  })
+
+  return {timezone, daily, hourly}
+}
+export const getFormateWeatherData = async (infoType) => {
+  const formateCurrentWeather = await getWeatherData(infoType).then(formatCurrentWeather)
+  const {lat, lon} = formateCurrentWeather
+
+  const formattedForecastWeather =
+    await getWeatherData(`onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,alerts&units=metric&`).then(formateForcastWeather)
+
+  return {...formateCurrentWeather, ...formattedForecastWeather}
+}
+export const formatToLocalTime = (sec, zone, format = "cccc dd LLL yyyy' | Local time: ' hh:mm a") => {
+  return DateTime.fromSeconds(sec).setZone(zone).toFormat(format)
+}
+export const iconUrlFromCode = (code) => `http://openweathermap.org/img/wn/${code}@2x.png`
